2685fb88a60aacdf0ab0cc92696ec4f28dac893b
laurennc <lcorlie@gmail.com>
2013-08-26 13:29:43 -0400

    changed to make plots in simulation time units instead of seconds

--- metal_evol_loop.py
+++ metal_evol_loop.py
@@ -4,7 +4,7 @@ mpl.use('agg')
 from lauren import *
 
 
-YEAR = 3.155693e+7 #s/yr
+#YEAR = 3.155693e+7 #s/yr
 
 pf = load('/u/10/l/lnc2115/home/WiseSimsData/DD0062/output_0062')
 halos = readhalos(foffile='/u/10/l/lnc2115/home/WiseSimsData/groups_02797.dat')
@@ -18,15 +18,15 @@ while (count < len(halos_to_run)):
 	radius, mass, center = r200(pf, halos['pos'][halonum,:],halos['mass'][halonum],verbose=False)
 	data_source = pf.h.sphere(center,radius/pf['cm'])
 	
-	fileout = '/u/10/l/lnc2115/home/WiseSims/analysis_plots/MDF/halo'+str(halonum)+'_stellarMDF.png'
+	fileout = '/u/10/l/lnc2115/home/WiseSims/analysis_plots/MDF/halo'+str(halonum)+'_stellarMDF_simtime.png'
 
 	sm, ct, metals = sfr_quants(data_source)
 	plot_stellar_t_vs_Z(pf,ct,metals,fileout)
 	
-	fileout2 = '/u/10/l/lnc2115/home/WiseSims/analysis_plots/MDF/halo'+str(halonum)+'_gasMDF.png'
-	if halonum != 22:
-		halo_metals = data_source['Metallicity']
-		metallicity_hist(metals,25,fileout2)
+#	fileout2 = '/u/10/l/lnc2115/home/WiseSims/analysis_plots/MDF/halo'+str(halonum)+'_gasMDF.png'
+#	if halonum != 22:
+#		halo_metals = data_source['Metallicity']
+#		metallicity_hist(metals,25,fileout2)
 
 	count = count + 1
 

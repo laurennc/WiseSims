2df9693ed674434dc532e4c88246b10a20f1cca5
laurennc <lcorlie@gmail.com>
2014-02-25 16:42:51 -0500

    added code to set limit on colorbar so you can see the spread in percentages at the low end. especially useful since the high end is very few cells. also added code that plots solid line at metallicity that would be predicted by a semi-analytic model, specifically M_metal/M_gas

--- fine_spaced_profiles.py
+++ fine_spaced_profiles.py
@@ -8,16 +8,21 @@ from lauren import *
 
 
 def build_radial_profile_matrix(pf,data_source,center,rvirKPC,bins):
-	metals = np.arange(101)*-6./100.
+	#metals = np.arange(101)*-6./100.
+	metals = np.linspace(0,-6,num=bins)
 	radii = distance_from_center(data_source['x'],data_source['y'],data_source['z'],center)*pf['kpc']
 	metallicities = make_solar_metallicity(data_source['Metallicity'])
+
+	#WANT TO CALCULATE METALLICITY THAT WOULD BE USED IN SAMS
+	sam_Z = make_solar_metallicity(np.sum(data_source['Metal_MassMsun'])/np.sum(data_source['CellMassMsun']))
+
 	#metal_masses = data_source['Metal_MassMsun']
 	#DOING VOLUMES BUT CALLED METAL_MASSES FOR EASE!
-	#metal_masses = data_source['CellVolume']/(pf['cm']**3.0)*(pf['pc']**3.0)
+	metal_masses = data_source['CellVolume']/(pf['cm']**3.0)*(pf['pc']**3.0)
 	#DOING DENSITY BUT CALLED METAL_MASSES FOR EASE!
 #	metal_masses = data_source['Density']
 	#DOING TOTAL GAS MASS BUT CALLED METAL_MASSES FOR EASE!
-	metal_masses = data_source['CellMassMsun']
+	#metal_masses = data_source['CellMassMsun']
 	dr = rvirKPC/bins
 	rp_r = np.arange(bins)*dr + dr/2.0
 	rp_vals = np.zeros((len(metals),bins))
@@ -28,6 +33,8 @@ def build_radial_profile_matrix(pf,data_source,center,rvirKPC,bins):
 		idx = np.where(metallicities[thisindex] > 0.)[0]
 		#LETS NORMALIZE BASED OFF THE TOTAL QUANTITY IN THAT ANNULUS
        		total_mass = np.sum(metal_masses[thisindex])
+		#LETS NOT NORMALIZE
+		#total_mass = 1.0
 		rp_vals[0,irad] = metal_masses[thisindex][idx].sum()
 		rp_vals[0,irad] = rp_vals[0,irad]/total_mass
 		i = 1
@@ -41,12 +48,13 @@ def build_radial_profile_matrix(pf,data_source,center,rvirKPC,bins):
 		rp_vals[i,irad] = metal_masses[thisindex][idx].sum()
 		rp_vals[i,irad] = rp_vals[i,irad]/total_mass
 
-	return rp_vals
+	return rp_vals, sam_Z
 
-def plot_rp_matrix(rp_vals,rvirKPC,fileout):
+def plot_rp_matrix(rp_vals,rvirKPC,sam_Z,fileout):
 	#plt.imshow(np.log10(rp_vals),cmap='spectral',interpolation='none',extent=(0,rvirKPC,-6,0),aspect='auto')
-	plt.imshow(rp_vals,cmap='spectral',interpolation='none',extent=(0,rvirKPC,-6,0),aspect='auto')
+	plt.imshow(rp_vals,cmap='spectral',interpolation='none',extent=(0,rvirKPC,-6,0),aspect='auto',vmax=0.3)
 	plt.colorbar()
+	plt.axhline(y=sam_Z,color='w',linewidth=2.5)
 	plt.xlabel('Radius (kpc)')
 	plt.ylabel('Metallicity') 
 	plt.savefig(fileout)
@@ -55,21 +63,26 @@ def plot_rp_matrix(rp_vals,rvirKPC,fileout):
 def run_many_halos(len):
 	pf = load('/u/10/l/lnc2115/vega/data/Wise/DD0062/output_0062')
 	data = cPickle.load(open('clump_dict.cpkl','rb'))
+	bins = 25.
 	i = 0
 	while i < len:
 		print 'i is ',i
 		data_source = pf.h.sphere(data['centers'][i],data['rvirs'][i]/pf['cm'])
 		rvirKPC = data['rvirs'][i]/pf['cm']*pf['kpc']
 
-		rpvals = build_radial_profile_matrix(pf,data_source,data['centers'][i],rvirKPC,100.)
+		rpvals, sam_Z = build_radial_profile_matrix(pf,data_source,data['centers'][i],rvirKPC,bins)
 		
 		#fileout = 'MassMetalProfiles/halo'+str(data['halonum'][i])+'_massmetalprofile_normal.png'
-		#fileout = 'VolMetalProfiles/halo'+str(data['halonum'][i])+'_volmetalprofile_normal.png'		
+		fileout = '/u/10/l/lnc2115/vega/data/Wise/Plots/VolMetalProfiles/bins25/halo'+str(data['halonum'][i])+'_volmetalprofile_25normal.png'		
 		#fileout = 'DensMetalProfiles/halo'+str(data['halonum'][i])+'_densmetalprofile_normal.png'
-		fileout = 'GasMetalProfiles/halo'+str(data['halonum'][i])+'_gasmetalprofile_normal.png'
+		#fileout = '/u/10/l/lnc2115/vega/data/Wise/Plots/GasMetalProfiles/bins25/halo'+str(data['halonum'][i])+'_gasmetalprofile_25normal.png'
 	
-		plot_rp_matrix(rpvals,rvirKPC,fileout)
+		plot_rp_matrix(rpvals,rvirKPC,sam_Z,fileout)
 		i = i + 1
+
+def gauss_function(x,a,mu,sigma):
+	return a*np.exp(-(x-mu)**2.0/(2.0*sigma**2.0))
+
 
 
 

aeacd66ba384e7c5f21bc43a9e24e2ebe6282376
laurennc <lcorlie@gmail.com>
2014-03-03 20:10:48 -0500

    added code to overplot the metallicity generally predicted by semi-analytic models

--- fine_spaced_profiles.py
+++ fine_spaced_profiles.py
@@ -18,21 +18,22 @@ def build_radial_profile_matrix(pf,data_source,center,rvirKPC,bins):
 
 	#metal_masses = data_source['Metal_MassMsun']
 	#DOING VOLUMES BUT CALLED METAL_MASSES FOR EASE!
-	metal_masses = data_source['CellVolume']/(pf['cm']**3.0)*(pf['pc']**3.0)
+	#metal_masses = data_source['CellVolume']/(pf['cm']**3.0)*(pf['pc']**3.0)
 	#DOING DENSITY BUT CALLED METAL_MASSES FOR EASE!
 #	metal_masses = data_source['Density']
 	#DOING TOTAL GAS MASS BUT CALLED METAL_MASSES FOR EASE!
-	#metal_masses = data_source['CellMassMsun']
+	metal_masses = data_source['CellMassMsun']
 	dr = rvirKPC/bins
 	rp_r = np.arange(bins)*dr + dr/2.0
 	rp_vals = np.zeros((len(metals),bins))
+	total_mass = np.sum(metal_masses)
 	for irad in range(int(bins)):
 		minrad = irad*dr
 		maxrad = minrad + dr
 		thisindex = (radii>=minrad) * (radii<maxrad)
 		idx = np.where(metallicities[thisindex] > 0.)[0]
 		#LETS NORMALIZE BASED OFF THE TOTAL QUANTITY IN THAT ANNULUS
-       		total_mass = np.sum(metal_masses[thisindex])
+       		#total_mass = np.sum(metal_masses[thisindex])
 		#LETS NOT NORMALIZE
 		#total_mass = 1.0
 		rp_vals[0,irad] = metal_masses[thisindex][idx].sum()
@@ -52,7 +53,7 @@ def build_radial_profile_matrix(pf,data_source,center,rvirKPC,bins):
 
 def plot_rp_matrix(rp_vals,rvirKPC,sam_Z,fileout):
 	#plt.imshow(np.log10(rp_vals),cmap='spectral',interpolation='none',extent=(0,rvirKPC,-6,0),aspect='auto')
-	plt.imshow(rp_vals,cmap='spectral',interpolation='none',extent=(0,rvirKPC,-6,0),aspect='auto',vmax=0.3)
+	plt.imshow(rp_vals,cmap='spectral',interpolation='none',extent=(0,rvirKPC,-6,0),aspect='auto')#,vmax=0.3)
 	plt.colorbar()
 	plt.axhline(y=sam_Z,color='w',linewidth=2.5)
 	plt.xlabel('Radius (kpc)')
@@ -73,9 +74,9 @@ def run_many_halos(len):
 		rpvals, sam_Z = build_radial_profile_matrix(pf,data_source,data['centers'][i],rvirKPC,bins)
 		
 		#fileout = 'MassMetalProfiles/halo'+str(data['halonum'][i])+'_massmetalprofile_normal.png'
-		fileout = '/u/10/l/lnc2115/vega/data/Wise/Plots/VolMetalProfiles/bins25/halo'+str(data['halonum'][i])+'_volmetalprofile_25normal.png'		
+		#fileout = '/u/10/l/lnc2115/vega/data/Wise/Plots/VolMetalProfiles/bins25/halo'+str(data['halonum'][i])+'_volmetalprofile_25totalmass.png'	
 		#fileout = 'DensMetalProfiles/halo'+str(data['halonum'][i])+'_densmetalprofile_normal.png'
-		#fileout = '/u/10/l/lnc2115/vega/data/Wise/Plots/GasMetalProfiles/bins25/halo'+str(data['halonum'][i])+'_gasmetalprofile_25normal.png'
+		fileout = '/u/10/l/lnc2115/vega/data/Wise/Plots/GasMetalProfiles/bins25/halo'+str(data['halonum'][i])+'_gasmetalprofile_25totalmass.png'
 	
 		plot_rp_matrix(rpvals,rvirKPC,sam_Z,fileout)
 		i = i + 1

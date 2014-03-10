1d90564a2dc94221f5f821d7c281e8ed25f851ff
laurennc <lcorlie@gmail.com>
2014-03-09 20:41:14 -0400

    produce quartile plots for the first 8 halos all on the same plot. easily modified to produce individual plots

--- fine_spaced_profiles.py
+++ fine_spaced_profiles.py
@@ -8,7 +8,6 @@ from lauren import *
 
 
 def build_radial_profile_matrix(pf,data_source,center,rvirKPC,metal_bins,radii_bins):
-	#metals = np.arange(101)*-6./100.
 	metals = np.linspace(0,-6,num=metal_bins)
 	radii = distance_from_center(data_source['x'],data_source['y'],data_source['z'],center)*pf['kpc']
 	metallicities = make_solar_metallicity(data_source['Metallicity'])
@@ -16,16 +15,14 @@ def build_radial_profile_matrix(pf,data_source,center,rvirKPC,metal_bins,radii_b
 	#WANT TO CALCULATE METALLICITY THAT WOULD BE USED IN SAMS
 	sam_Z = make_solar_metallicity(np.sum(data_source['Metal_MassMsun'])/np.sum(data_source['CellMassMsun']))
 
-	#metal_masses = data_source['Metal_MassMsun']
 	#DOING VOLUMES BUT CALLED METAL_MASSES FOR EASE!
 	#metal_masses = data_source['CellVolume']/(pf['cm']**3.0)*(pf['pc']**3.0)
-	#DOING DENSITY BUT CALLED METAL_MASSES FOR EASE!
-#	metal_masses = data_source['Density']
 	#DOING TOTAL GAS MASS BUT CALLED METAL_MASSES FOR EASE!
 	metal_masses = data_source['CellMassMsun']
 	dr = rvirKPC/radii_bins
 	rp_r = np.arange(radii_bins)*dr + dr/2.0
 	rp_vals = np.zeros((metal_bins,radii_bins))
+	#NORMALIZE BY THE TOTAL QUANTITY
 #	total_mass = np.sum(metal_masses)
 	for irad in range(int(radii_bins)):
 		minrad = irad*dr
@@ -64,62 +61,70 @@ def plot_rp_matrix(rp_vals,rvirKPC,sam_Z,fileout):
 def run_many_halos(len):
 	pf = load('/u/10/l/lnc2115/vega/data/Wise/DD0062/output_0062')
 	data = cPickle.load(open('clump_dict.cpkl','rb'))
-	radii_bins = 25.
-	metal_bins = 500.
+	radii_bins = 20.
+	metal_bins = 700.
 	i = 0
+	iax = 331
+	fig = plt.figure(figsize=(10,10))
 	while i < len:
 		print 'i is ',i
 		data_source = pf.h.sphere(data['centers'][i],data['rvirs'][i]/pf['cm'])
 		rvirKPC = data['rvirs'][i]/pf['cm']*pf['kpc']
 
-		rpvals, sam_Z = build_radial_profile_matrix(pf,data_source,data['centers'][i],rvirKPC,mtal_bins,radii_bins)
+		rpvals, sam_Z = build_radial_profile_matrix(pf,data_source,data['centers'][i],rvirKPC,metal_bins,radii_bins)
 		
-	#fileout = 'MassMetalProfiles/halo'+str(data['halonum'][i])+'_massmetalprofile_normal.png'
-		#fileout = '/u/10/l/lnc2115/vega/data/Wise/Plots/VolMetalProfiles/bins25/halo'+str(data['halonum'][i])+'_volmetalprofile_25totalmass.png'	
-		#fileout = 'DensMetalProfiles/halo'+str(data['halonum'][i])+'_densmetalprofile_normal.png'
-		fileout = '/u/10/l/lnc2115/vega/data/Wise/Plots/GasMetalProfiles/bins25/halo'+str(data['halonum'][i])+'_gasmetalprofile_25totalmass.png'
-	
-		plot_rp_matrix(rpvals,rvirKPC,sam_Z,fileout)
+		per_vals = find_percentile_values(rpvals,0.25,0.75,metal_bins,radii_bins,0.02)
+		print 'iax is ',iax
+		ax1 = fig.add_subplot(iax)
+		fileout = 'Z_rp_quartiles_halo'+str(data['halonum'][i])+'.png'
+		plot_percentile_values(ax1,per_vals,data['halonum'][i],rvirKPC,radii_bins,fileout)	
+
+		#plot_rp_matrix(rpvals,rvirKPC,sam_Z,fileout)
 		i = i + 1
+		iax = iax + 1
+	fileout = 'Z_rp_quartiles.png'
+	plt.savefig(fileout)
+	#plt.close()
+
+	return
 
 
 def find_percentile_values(rp,lower,upper,metal_bins,radii_bins,tolerance):
-	#What I Want This Code to Do!
-	#Need the normalized rp values because then the percentages make sense!
-	# For each radial bin, I want to 
+	#Using the normalized radial profiles, find each percentile and the median to plot instead of the heat map	
 	metals = np.linspace(0,-6,num=metal_bins)
 	per_vals = np.zeros((3,radii_bins))
 	for idr in range(int(radii_bins)):
 		sum = 0.0
-		for i in range(int(metal_bins)-1):
-			i = i + 1
+		if (rp[0,idr] > lower):
+			per_vals[0,idr] = metals[0]
+		for i in range(int(metal_bins)): #-1):
+			#i = i + 1
 			sum = sum + rp[i,idr]
 			if (sum <= lower+tolerance) and (sum >= lower-tolerance):
-				#print 'lower',i, sum
 				per_vals[0,idr] = metals[i]
 			elif (sum <= 0.5+tolerance) and (sum >= 0.5-tolerance):
-				#print 'median',i, sum
 				per_vals[1,idr] = metals[i]
 			elif (sum <= upper+tolerance) and (sum >= upper-tolerance):
-				#print 'upper',i, sum
 				per_vals[2,idr] = metals[i]
 			else:
 				sum = sum
+		if per_vals[2,idr] == 0.0:
+			per_vals[2,idr] = metals[metal_bins-1]	
 	return per_vals
 	
 
-def plot_percentile_values(per_vals,rvirKPC,radii_bins,fileout):
-	#NEED TO FILL IN HOW WHAT PARAMETERS I NEED TO PAS BUT FOR NOW.....
-	#LET'S ASSUME THAT I HAVE PER_VALS
-	#x = rp_r
+def plot_percentile_values(ax1,per_vals,halonum,rvirKPC,radii_bins,fileout):
 	#y_low = per_vals[0,*], y_med=per_vals[1,*],y_high=per_vals[2,*]
 	dr = rvirKPC/radii_bins
 	rp_r = np.arange(radii_bins)*dr + dr/2.0
-	plt.plot(rp_r,per_vals[0,:],'k')
-	plt.plot(rp_r,per_vals[2,:],'k')
-	plt.plot(rp_r,per_vals[1,:],'k',lw=2.5)
-	plt.fill_between(rp_r,per_vals[0,:],per_vals[2,:],color='grey',alpha='0.5')
-	plt.save(fileout)
+	ax1.plot(rp_r,per_vals[0,:],'k')
+	ax1.plot(rp_r,per_vals[2,:],'k')
+	ax1.plot(rp_r,per_vals[1,:],'k',lw=2.5)
+	ax1.fill_between(rp_r,per_vals[0,:],per_vals[2,:],color='grey',alpha='0.5')
+	ax1.text(0.2,-5,'Halo '+str(halonum))
+	ax1.set_ylim(-6,0)
+	#plt.savefig(fileout)
+	#plt.close()
 	return
 
 

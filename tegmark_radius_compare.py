6bab106313527f5d2e21f23f223a611cbd4b0d8a
laurennc <lcorlie@gmail.com>
2014-03-20 19:56:24 -0400

    had added code where the error bars were defined using loops but thats not the right way to include them!

--- tegmark_radius_compare.py
+++ tegmark_radius_compare.py
@@ -2,9 +2,10 @@ from lauren import *
 
 data = cPickle.load(open('/u/10/l/lnc2115/vega/repos/WiseSims/clump_dict.cpkl','rb'))
 
-grp1 = [0,1,14,15,19,20,23]
-grp2 = [4,6,7,11,21]
-grp3 = [5,13]
+#grp1 = [0,1,14,15,19,20,23]
+#grp2 = [4,6,7,11,21]
+#grp3 = [5,13]
+grps = [1,14,15,19,20,23,4,6,7,11,21,13]
 maxs = [0,4,5]
 #independent
 indp = [2,3,8,9,10,16,17,22]
@@ -25,33 +26,34 @@ r_scale = return_snwinds_scaled_radii(data['t_start'],data['star_masses'])
 lauren_r = [2.78808,2.83108,2.71597,1.58790,2.22833,2.08742,1.48833,1.48833,0.292994,0.284480,1.27494,2.50799,0.106331,1.69834,2.65037,1.34190,0.837878,0.730189,0.309501,0.913325,0.582502,1.48833,0.922523,0.553023]
 
 ravg = data['avgRadius']/r_scale
-rmax = data['maxRadius']/r_scale
+rmin = data['zMinRadius']/r_scale
+rmax = data['zMaxRadius']/r_scale
 
 ravgcomp = ravg/lauren_r
 rmaxcomp = rmax/lauren_r
+rmincomp = rmin/lauren_r
+
+#errsMAXS = np.zeros(len(maxs)*2)
+#count = 0
+#for i in range(len(maxs)):
+#        errsMAXS[count] = rmin[maxs[i]]
+#        count = count + 1
+#        errsMAXS[count] = rmax[maxs[i]]
+#        count = count + 1
+
 
 plt.plot(np.log10(data['star_masses'])[indp],ravgcomp[indp],'rs',label='Indp')
-plt.plot(np.log10(data['star_masses'])[grp1],ravgcomp[grp1],'bo',label='Groups')
-plt.plot(np.log10(data['star_masses'])[grp2],ravgcomp[grp2],'bo')
-plt.plot(np.log10(data['star_masses'])[grp3],ravgcomp[grp3],'bo')
+plt.errorbar(np.log10(data['star_masses'])[indp],ravgcomp[indp],xerr=errsINDP,color='red')
+plt.plot(np.log10(data['star_masses'])[grps],ravgcomp[grps],'bo',label='Groups')
+plt.errorbar(np.log10(data['star_masses'])[grps],ravgcomp[grps],xerr=errsGRPS,color='blue')
 plt.plot(np.log10(data['star_masses'])[maxs],ravgcomp[maxs],'g*',markersize=13.5,label='Max Grp Mem')
+plt.errorbar(np.log10(data['star_masses'])[maxs],ravgcomp[maxs],xerr=errsMAXS,color='green')
 #plt.plot(np.arange(6)+2.5,np.zeros(6)+1,'k--',linewidth=1.5)
 plt.axhline(y=1,linewidth=1.5,linestyle='--',color='k')
 plt.xlabel('log(Stellar Mass)')
 plt.ylabel('R Avg / Tegmark')
 plt.legend()
-plt.savefig('best_tegmark_compare_avg_fixed.eps')
+plt.savefig('best_tegmark_compare_avg_fixed_werrs.eps')
 plt.close()
 
-plt.plot(np.log10(data['star_masses'])[indp],rmaxcomp[indp],'rs',label='Indp')
-plt.plot(np.log10(data['star_masses'])[grp1],rmaxcomp[grp1],'bo',label='Groups')
-plt.plot(np.log10(data['star_masses'])[grp2],rmaxcomp[grp2],'bo')
-plt.plot(np.log10(data['star_masses'])[grp3],rmaxcomp[grp3],'bo')
-plt.plot(np.log10(data['star_masses'])[maxs],rmaxcomp[maxs],'g*',markersize=13.5,label='Max Grp Mem')
-#plt.plot(np.arange(6)+2.5,np.zeros(6)+1,'k--',linewidth=1.5)
-plt.axhline(y=1,linewidth=1.5,linestyle='--',color='k')
-plt.xlabel('log(Stellar Mass)')
-plt.ylabel('R Max / Tegmark')
-plt.savefig('best_tegmark_compare_max_fixed.eps')
-plt.close()
 

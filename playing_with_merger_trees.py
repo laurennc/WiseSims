92568fc811cdd7a1301d0a88185085fa1b067289
laurennc <lcorlie@gmail.com>
2014-05-20 15:42:12 -0400

    no real serious code yet. inteded for what its named as

---
+++ playing_with_merger_trees.py
@@ -0,0 +1,10 @@
+from lauren import *
+from /u/10/l/lnc2115/vega/codes/rockstar-dot/rockstar-plot import *
+
+fn = '/u/10/l/lnc2115/vega/data/Wise/rockstar_halos/trees/tree_0_0_0.dat'
+pf_store = '/u/10/l/lnc2115/vega/data/Wise/rockstar_halos/pfs.txt'
+hid, data = load_tree(0,fn)
+
+
+
+
